0767ab11cd3c5111c1ec5bbfcafa835324badb67
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-08-14 20:25:43 +0000

    Storing hours, minutes and seconds in variables

--- digitalclock/script.js
+++ digitalclock/script.js
@@ -2,3 +2,11 @@ const hourEl = document.getElementById('hours')
 const minuteEl = document.getElementById('minutes')
 const secondEl = document.getElementById('seconds')
 
+function updateClock(){
+	let h = new Date().getHours()
+	let m = new Date().getMinutes()
+	let s = new Date().getSeconds()
+
+}
+
+updateClock()
