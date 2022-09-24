a9e18712acab2761c9b6cd6733581bbe6952805a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-09-24 10:21:50 +0000

    QuestionEl is modified with js with the random numbers generated

--- Multiplier/index.js
+++ Multiplier/index.js
@@ -1,8 +1,11 @@
 //Getting elements
 const questionEl = document.getElementById('question');
 
+
 //Generating random numbers
 const num1 = Math.floor(Math.random() * 10);
 const num2 = Math.floor(Math.random() * 10);
-console.log(num1);
-console.log(num2);
+
+
+//InnerText
+questionEl.innerText = `What is ${num1} multiplied by ${num2}?`
