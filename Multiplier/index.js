84d95dd06feed960fc5f0d97d58bfb6efac6891c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-09-24 10:42:08 +0000

    Switching the positions of  random numbers and elements

--- Multiplier/index.js
+++ Multiplier/index.js
@@ -1,14 +1,15 @@
+//Generating random numbers
+const num1 = Math.floor(Math.random() * 10);
+const num2 = Math.floor(Math.random() * 10);
+//Calculating the answer
+const correctAns = num1 * num2;
+
 //Getting elements
 const questionEl = document.getElementById('question');
 const formEL = document.getElementById('form');
 const inputEl = document.getElementById('input')
 const scoreEl = document.getElementById('score');
 
-//Generating random numbers
-const num1 = Math.floor(Math.random() * 10);
-const num2 = Math.floor(Math.random() * 10);
-//Calculating the answer
-const correctAns = num1 * num2;
 
 let score = JSON.parse(localStorage.getItem('score'));
 
