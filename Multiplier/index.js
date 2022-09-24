e37bc0b7c3f16dc9e52e0ab509aae585b8cb990b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-09-24 10:19:09 +0000

    Getting elements and generating num2

--- Multiplier/index.js
+++ Multiplier/index.js
@@ -3,4 +3,6 @@ const questionEl = document.getElementById('question');
 
 //Generating random numbers
 const num1 = Math.floor(Math.random() * 10);
+const num2 = Math.floor(Math.random() * 10);
 console.log(num1);
+console.log(num2);
