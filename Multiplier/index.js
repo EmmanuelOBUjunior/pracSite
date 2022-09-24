38b19adc13c8c53b2a1505901e1bd5c37853a9dd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-09-24 10:33:00 +0000

    Working on storing marks in the local storage

--- Multiplier/index.js
+++ Multiplier/index.js
@@ -12,8 +12,22 @@ const correctAns = num1 * num2;
 //InnerText
 questionEl.innerText = `What is ${num1} multiplied by ${num2}?`
 
+let score = JSON.parse(localStorage.getItem('score'));
+
+if(!score){
+	score = 0
+}
 
 formEL.addEventListener('submit', ()=>{
 	const userAns = +inputEl.value;
-	console.log(userAns, typeof(userAns) );
+	if(userAns ===	correctAns){
+		score++
+	}
+	else{
+		score--
+	}
 })
+
+function storeScore(){
+	localStorage.setItem('score', JSON.stringify(score))
+}
