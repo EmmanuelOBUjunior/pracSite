f0cace56e43a6c12a7feea805e922cae6191d509
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-09-24 10:23:18 +0000

    getting form element by ID

--- Multiplier/index.js
+++ Multiplier/index.js
@@ -1,6 +1,6 @@
 //Getting elements
 const questionEl = document.getElementById('question');
-
+const formEL = document.getElementById('form');
 
 //Generating random numbers
 const num1 = Math.floor(Math.random() * 10);
@@ -9,3 +9,5 @@ const num2 = Math.floor(Math.random() * 10);
 
 //InnerText
 questionEl.innerText = `What is ${num1} multiplied by ${num2}?`
+
+
