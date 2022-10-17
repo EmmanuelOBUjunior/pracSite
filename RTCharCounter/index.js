29512484d4626561e6a63897c2d712c8effe7c68
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-10-17 07:47:27 +0000

    Getting the attribute maxlength from textarea

--- RTCharCounter/index.js
+++ RTCharCounter/index.js
@@ -1,11 +1,15 @@
 const textAreaEl = document.getElementById('textarea');
 const totalCharEl = document.getElementById('total-counter')
+const remCharEl = document.getElementById('remaining-counter')
 
 
 textAreaEl.addEventListener("keyup", ()=>{
+	console.log(textAreaEl.getAttribute('maxlength'))
 	updateCounter();
+	// console.log(textAreaEl.getAttribute('maxlength'))
 })
 
 function updateCounter() {
 	totalCharEl.innerText = textAreaEl.value.length
+
 }
