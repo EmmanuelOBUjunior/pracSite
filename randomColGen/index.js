fabb06d4eec9ed6f2d5f7412a636816807b039fb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-10-22 10:09:58 +0000

    Creating generate random colors function

--- randomColGen/index.js
+++ randomColGen/index.js
@@ -7,9 +7,9 @@ for (let index = 0; index < 30; index++) {
 	containerEl.appendChild(colorContainer)
 }
 
+function generateRandomColors(){
 
-
-randomNumber()
+}
 
 function randomNumber(){
 	const chars = "0123456789abcdef";
