f87a899f4a477db47cecb65a4d635aff5880bef1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-10-22 09:52:17 +0000

    Appended the colorContainer to the main container

--- randomColGen/index.js
+++ randomColGen/index.js
@@ -3,4 +3,6 @@ const containerEl = document.querySelector(".container")
 for (let index = 0; index < 30; index++) {
 	const colorContainer = document.createElement("div")
 	colorContainer.classList.add("color-container")
+
+	containerEl.appendChild(colorContainer)
 }
