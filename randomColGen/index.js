982681b027545d4b8cdaf42da3c969efb8e9452e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-10-22 11:16:00 +0000

    Done with color generator

--- randomColGen/index.js
+++ randomColGen/index.js
@@ -14,21 +14,21 @@ generateColors();
 function generateColors(){
 	colorContainerEls.forEach(colorContainerEl => {
 		const newColor = randomColor()
-		colorContainerEl.style.backgroundColor = "#"+ newColor;
+		colorContainerEl.style.backgroundColor = newColor;
 		console.log(newColor)
 
-		colorContainerEl.innerText = "#"+newColor;
+		colorContainerEl.innerText = newColor;
 	});
 	
 }
 
 function randomColor(){
 	const chars = "0123456789abcdef";
-	let colorCode = " ";
+	let colorCode = "";
 
 	for (let index = 0; index < 6; index++) {
 		const randomNum = Math.floor(Math.random() * chars.length)
 		colorCode += chars.substring(randomNum, randomNum + 1);
 	}
-	return colorCode;
+	return "#"+colorCode;
 }
