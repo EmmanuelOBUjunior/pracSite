3090124b019ad1ad8d7f184d9fc237c20c9c5bb0
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-10-22 09:51:16 +0000

    Added a class to the div

--- randomColGen/index.js
+++ randomColGen/index.js
@@ -2,5 +2,5 @@ const containerEl = document.querySelector(".container")
 
 for (let index = 0; index < 30; index++) {
 	const colorContainer = document.createElement("div")
-	
+	colorContainer.classList.add("color-container")
 }
