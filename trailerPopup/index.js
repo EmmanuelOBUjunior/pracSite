09e4ce176fea2ed2b7ee97c7d88f0b7601fedc61
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2022-12-12 08:30:16 +0000

    Created an event listener for btnEl

--- trailerPopup/index.js
+++ trailerPopup/index.js
@@ -2,3 +2,6 @@ const btnEl = document.querySelector(".btn")
 const closeBtnEl = document.querySelector(".close-icon")
 const trailerEL	 = document.querySelector(".trailer-container")
 
+btnEl.addEventListener("click", ()=>{
+	
+})
