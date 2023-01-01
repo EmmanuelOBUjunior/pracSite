4a3ad8e9e226600fd391918cc09c2e9c2df7dce1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-01 18:56:36 +0000

    Targeting video class

--- trailerPopup/index.js
+++ trailerPopup/index.js
@@ -1,6 +1,7 @@
 const btnEl = document.querySelector(".btn")
 const closeBtnEl = document.querySelector(".close-icon")
 const trailerEL	 = document.querySelector(".trailer-container")
+const videoEl = document.querySelector(".video")
 
 btnEl.addEventListener("click", ()=>{
 	trailerEL.classList.remove("active")
