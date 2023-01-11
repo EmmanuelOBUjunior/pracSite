2d56101eee1a576806e06619299d21a393bcbc7b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-11 08:00:39 +0000

    Added an event listener

--- blurredBackgroundPopUp/index.js
+++ blurredBackgroundPopUp/index.js
@@ -1,3 +1,7 @@
 const  btnEl = document.querySelector(".btn")
 const popupContainerEl = document.querySelector(".popup-container")
 const mainContainerEl = document.querySelector(".main-container")
+
+btnEl.addEventListener("click", () => {
+	popupContainerEl.classList.remove("active")
+})
