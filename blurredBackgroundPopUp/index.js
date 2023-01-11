bc63032d5a14aeb288ee92bb97b056c0adb02c20
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-11 08:02:11 +0000

    Added active class to main-container on btn click event

--- blurredBackgroundPopUp/index.js
+++ blurredBackgroundPopUp/index.js
@@ -3,5 +3,5 @@ const popupContainerEl = document.querySelector(".popup-container")
 const mainContainerEl = document.querySelector(".main-container")
 
 btnEl.addEventListener("click", () => {
-	popupContainerEl.classList.remove("active")
+	mainContainerEl.classList.add("active")
 })
