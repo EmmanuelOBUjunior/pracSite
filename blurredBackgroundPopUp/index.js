4e7d89e2979c793bcc9e0a6b3c4a96f2ca517ddc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-11 08:11:56 +0000

    pointing to close-icon

--- blurredBackgroundPopUp/index.js
+++ blurredBackgroundPopUp/index.js
@@ -1,6 +1,8 @@
 const  btnEl = document.querySelector(".btn")
 const popupContainerEl = document.querySelector(".popup-container")
 const mainContainerEl = document.querySelector(".main-container")
+const closeIconEl = document.querySelector(".close-icon");
+
 
 btnEl.addEventListener("click", () => {
 	mainContainerEl.classList.add("active");
