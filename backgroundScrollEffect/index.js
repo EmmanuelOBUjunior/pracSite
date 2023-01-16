32676f05ad95793378a25cc9e5b238cfe0e6b6e2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-16 08:46:35 +0000

    Fixed: background opacity on scroll effect

--- backgroundScrollEffect/index.js
+++ backgroundScrollEffect/index.js
@@ -1,11 +1,12 @@
 const bgImageEl = document.getElementById('bg-image');
 
 window.addEventListener("scroll", ()=>{
+	console.log("scrolling")
 	updateBackgroundImage();
 })
 
 
-function updateBacgroundImage() {
+function updateBackgroundImage() {
 	bgImageEl.style.opacity = 1 - window.pageYOffset/900;
 }
 
