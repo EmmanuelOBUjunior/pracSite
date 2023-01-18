469520a8fce1c173a594eddd1009fadba9f9435d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-18 08:15:58 +0000

    Created button element and appended it to the container element

--- drumKit/script.js
+++ drumKit/script.js
@@ -3,7 +3,9 @@ const kits = ["crash","kick","snare","tom"]
 const containerEl = document.querySelector(".container")
 
 kits.forEach(kit =>{
-	const buttonEl = document.createElement("btn")
+	const buttonEl = document.createElement("button")
+	buttonEl.classList.add("btn")
+	containerEl.appendChild(buttonEl)
 })
 
 
