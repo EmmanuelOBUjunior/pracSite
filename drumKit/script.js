5e0dee47184bc9e53c166547216622d8c1942e12
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-19 07:34:00 +0000

    Appended audio element to containerEL

--- drumKit/script.js
+++ drumKit/script.js
@@ -5,9 +5,9 @@ const containerEl = document.querySelector(".container")
 kits.forEach(kit =>{
 	const buttonEl = document.createElement("button")
 	buttonEl.classList.add("btn")
-	containerEl.appendChild(buttonEl)
 	buttonEl.innerText  = kit;
 	buttonEl.style.backgroundImage = "url(images/"+kit+".png)"
+	containerEl.appendChild(buttonEl)
 	const audioEl = document.createElement("audio");
 	audioEl.src = "sounds/" + kit + ".mp3";
 	containerEl.appendChild(audioEl);
