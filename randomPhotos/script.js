461774fc4a479e69d1d68e58da80e6c6d964a5b5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-01-21 22:06:56 +0000

    Added a source to the image element

--- randomPhotos/script.js
+++ randomPhotos/script.js
@@ -9,7 +9,7 @@ btnEl.addEventListener("click", () =>{
 function createImages() {
 	for (let index = 0; index < noOfImages; index++) {
 		const imgEl = document.createElement("img")
-		imgEl.src = ``
+		imgEl.src = `https://picsum.photos/200/300?random=${Math.floor(Math.random()*2000)}`
 	}
 	
 }
