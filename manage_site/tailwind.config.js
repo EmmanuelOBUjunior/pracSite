9d361d6107634e09a9038943cf71c24e79985e99
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-03-17 08:36:41 +0000

    Add very light gray to config file

--- manage_site/tailwind.config.js
+++ manage_site/tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(223, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
       }
     },
   },
