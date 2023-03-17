1a518e0b03eab26a81156c0ee354791b7ddc38b2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-03-17 08:09:08 +0000

    Added darkBlue color to config file

--- manage_site/tailwind.config.js
+++ manage_site/tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         brightRed: 'hsl(12, 88%, 59%)',
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%',
+        
       }
     },
   },
