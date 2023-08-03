a43f6504e0078ce193f157024fa5aa037bf62119
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-03 16:13:24 +0000

    Creating form

--- do-this/src/App.jsx
+++ do-this/src/App.jsx
@@ -3,6 +3,7 @@ import './styles.css'
 function App() {
 
   return (
+    <>
     <form className="new-item-form">
       <div className="form-row">
       <label htmlFor="item">New Item</label>
@@ -10,6 +11,11 @@ function App() {
       </div>
       <button className="btn">Add Item</button>
     </form>
+    <h1 className="header">Todo List</h1>
+    <ul className='list'>
+      <li></li>
+    </ul>
+    </>
   )
 }
 
