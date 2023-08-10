0c73ed439b0c9d48e7a5de8a271a231409acd156
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-10 20:16:47 +0000

    Working on hooks

--- moviexp/src/App.jsx
+++ moviexp/src/App.jsx
@@ -4,9 +4,13 @@ import {useState} from 'react'
 
 const App = () =>{
   const [counter, setCounter] = useState(0)
+
+  const changeCount=()=>{
+    setCounter((prevCount) => prevCount -1)
+  } 
   return (
     <div className="App">
-     <button onClick={() => setCounter((prevCount)=> prevCount - 1)}>-</button>
+     <button onClick={changeCount}>-</button>
      <h1>{counter}</h1>
      <button onClick={() => setCounter((prevCount)=> prevCount + 1)}>+</button>
     </div>
