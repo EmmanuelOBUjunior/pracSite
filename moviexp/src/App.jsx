38daa82ad69d9eb5b4363767ec48a1a5679fc8e7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-10 19:59:59 +0000

    Created Person component

--- moviexp/src/App.jsx
+++ moviexp/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 
 
-const Person = ({firstName, LastName, Age}) =>{
+const Person = ({firstName, lastName, Age}) =>{
   return(
     <>
     <h1>First Name: {firstName}</h1>
     <h2>Last Name: {lastName}</h2>
-    <h2>Age: Age</h2>
+    <h2>Age: {Age}}</h2>
     </>
   )
 }
