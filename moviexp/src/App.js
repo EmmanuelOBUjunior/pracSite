e4f3b21c009e775a37c201d56c6409301b44fe9b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-10 19:50:38 +0000

    Cleaning up app file

--- moviexp/src/App.js
+++ moviexp/src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
 
 const App = () =>{
-  const name = "Developer"
+  const name = "null"
   const isShowing = true;
   return (
     <div className="App">
-      <h1>Hello, {isShowing ? name : "Unknown"}</h1>
-      {name ? 
-      <>
-      Hello, {name}
-      </>:
-      <>
-      Hello, Unknown
-      </>}
     </div>
   );
 }
