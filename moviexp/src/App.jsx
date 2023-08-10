403836eb4c75c1d7fd17475310d675507dda8371
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-10 20:02:10 +0000

    Removed Person component

--- moviexp/src/App.jsx
+++ moviexp/src/App.jsx
@@ -1,17 +1,6 @@
 import './App.css';
 
 
-const Person = ({firstName, lastName, Age}) =>{
-  return(
-    <>
-    <h1>First Name: {firstName}</h1>
-    <h2>Last Name: {lastName}</h2>
-    <h2>Age: {Age}</h2>
-    </>
-  )
-}
-
-
 
 const App = () =>{
   return (
