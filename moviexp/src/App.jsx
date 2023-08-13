d8f0edad9207637361cafe08bf59db97b4fb5fde
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-13 19:36:56 +0000

    Starting MoviExp

--- moviexp/src/App.jsx
+++ moviexp/src/App.jsx
@@ -4,9 +4,15 @@ import {useEffect} from 'react'
 const API_URL = 'https://www.omdbapi.com?apikey=29220acb'
 const App = () =>{
    
+    const searchMovies = async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = response.json()
+    }
+
     useEffect(()=>{
 
     }, [])
+
     return(
         <h1>App</h1>
     )
