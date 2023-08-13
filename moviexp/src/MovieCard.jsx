eb94c26672f09698e3f94b18dd5dd65e6208ca22
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-13 21:25:38 +0000

    Working on movie card

--- moviexp/src/MovieCard.jsx
+++ moviexp/src/MovieCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const MovieCard = () => {
+const MovieCard = ({Title, Year, Poster}) => {
   return (
     <>
     <div className="movie">
                     <div>
-                        <p>{movie1.Year}</p>
+                        <p>{Year}</p>
                     </div>
                     <div>
                     <img src={movie1.Poster !== "N/A" ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
