9c10e66cac264d6c66a7a116b23d703fafd86e92
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-08-13 21:59:28 +0000

    Working on search input field

--- moviexp/src/App.jsx
+++ moviexp/src/App.jsx
@@ -10,6 +10,7 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=29220acb'
 
 const App = () =>{
     const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
    
     const searchMovies = async(title)=>{
         const response = await fetch(`${API_URL}&s=${title}`)
